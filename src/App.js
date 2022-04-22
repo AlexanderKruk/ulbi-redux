aabe1cd52556e48ad32db025ea6e4bacea2e1575
Alexander Kruk <alexander.kruk@gmail.com>
2022-04-22 16:14:09 +0200

    added reducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  return <div>Work!</div>;
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  const handlerAdd = (payload) => dispatch({ type: "ADD", payload });
+  const handlerDelete = (payload) => dispatch({ type: "DEL", payload });
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => handlerAdd(Number(prompt()))}>ADD</button>
+      <button onClick={() => handlerDelete(Number(prompt()))}>DEL</button>
+    </div>
+  );
 }
 
 export default App;
