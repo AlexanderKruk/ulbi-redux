723bda8a7fa8c99f0722e3da6d51a399dade1c4a
Alexander Kruk <alexander.kruk@gmail.com>
2022-04-25 17:19:56 +0200

    added customer reducer and refactor code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
+import { addCustomerAction, delCustomerAction } from "./store/customerReducer";
 
 function App() {
   const count = useSelector((state) => state.cash.count);
+  const customer = useSelector((state) => state.customer.customer);
   const dispatch = useDispatch();
 
   const handlerAdd = (payload) => dispatch({ type: "ADD", payload });
   const handlerDelete = (payload) => dispatch({ type: "DEL", payload });
+
+  const handlerAddCustomer = (name) => {
+    dispatch(addCustomerAction({ name, id: Date.now() }));
+  };
+
+  const handlerDeleteCustomer = (id) => dispatch(delCustomerAction(id));
 
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={() => handlerAdd(Number(prompt()))}>ADD</button>
       <button onClick={() => handlerDelete(Number(prompt()))}>DEL</button>
+      <button onClick={() => handlerAddCustomer(prompt())}>Add Customer</button>
+      {customer.length > 0 ? (
+        customer.map((item) => (
+          <div
+            key={item.id}
+            style={{ border: "1px solid", margin: "5px", padding: "5px" }}
+            onClick={() => handlerDeleteCustomer(item.id)}
+          >
+            {item.name}
+          </div>
+        ))
+      ) : (
+        <div>Clients not exists</div>
+      )}
+      <div></div>
     </div>
   );
 }
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -2,15 +2,30 @@ const initialState = {
   customer: [],
 };
 
+const ADD_CUSTOMER = "ADD_CUSTOMER";
+const DEL_CUSTOMER = "DEL_CUSTOMER";
+
 export const customerReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_CUSTOMER": {
-      return { ...state, customer: [...state.customer, ...action.payload] };
+    case ADD_CUSTOMER: {
+      return { ...state, customer: [...state.customer, action.payload] };
     }
-    case "DEL_CUSTOMER": {
-      return { ...state, count: state.customer };
+    case DEL_CUSTOMER: {
+      return {
+        ...state,
+        customer: state.customer.filter((item) => action.payload !== item.id),
+      };
     }
     default:
       return state;
   }
 };
+
+export const addCustomerAction = (payload) => ({
+  type: ADD_CUSTOMER,
+  payload,
+});
+export const delCustomerAction = (payload) => ({
+  type: DEL_CUSTOMER,
+  payload,
+});
